823705a7ddc9ccb5633cb54569867f87dd647ab2
ericdalling <dalling.eric@gmail.com>
2017-09-14 23:00:45 -0600

    fixed security group

--- devops/terraform/modules/ecs-service/alb.tf
+++ devops/terraform/modules/ecs-service/alb.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "alb_sg" {
 }
 
 resource "aws_security_group_rule" "alb_sg_ingress_80" {
-  security_group_id = "${module.ecs-service.alb_sg_id}"
+  security_group_id = "${aws_security_group.alb_sg.id}"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
